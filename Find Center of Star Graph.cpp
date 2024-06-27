4997926efcac580f10ebcf78eb6ebaa57058b2a5
Ankita Nadarge <146661081+nadargeAnkita@users.noreply.github.com>
2024-06-27 18:44:11 +0530

    Create Find Center of Star Graph.cpp

---
+++ Find Center of Star Graph.cpp
@@ -0,0 +1,9 @@
+#include<iosttream>
+using namespace std;
+
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& e) {
+    return e[0][0] == e[1][0] || e[0][0] == e[1][1] ? e[0][0] : e[0][1];
+}
+};
